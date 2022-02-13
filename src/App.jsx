407b71f3159d78e39a191a6460600ed63b80b9d2
VitaPavlenko <91799017+VitaPavlenko@users.noreply.github.com>
2022-02-13 16:08:12 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,139 @@
 import Button from 'components/Button/Button';
 import Loader from 'components/Loader/Loader';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ImageGallery from './components/ImageGallery/ImageGallery';
 import Searchbar from './components/Searchbar/Searchbar';
 import getFetch from './server/getFeach';
 import Modal from 'components/Modal/Modal';
 import s from './App.module.css';
 const Status = {
-  IDLE: 'idle',
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
-
-  //  - idle - запроса еще нет
-  //   - pending - пошел запрос
-  //   - resolved - успешный запрос
-  //   - rejected - запрос с ошибкой
+  IDLE: 'idle', // запроса еще нет
+  PENDING: 'pending', // пошел запрос
+  RESOLVED: 'resolved', // успешный запрос
+  REJECTED: 'rejected', // запрос с ошибкой
 };
-export default class App extends Component {
-  state = {
-    hits: [],
-    newImput: 'cat',
-    error: null,
-    status: Status.IDLE,
-    page: 1,
-    showModal: false,
-    imageURL: '',
-  };
+export default function App() {
+  // state = {
+  //   hits: [],
+  //   newImput: 'cat',
+  //   error: null,
+  //   status: Status.IDLE,
+  //   page: 1,
+  //   showModal: false,
+  //   imageURL: '',
+  // };
 
-  componentDidMount() {
-    getFetch(this.state.newImput, this.state.page).then(data =>
-      this.setState({ hits: [...data.hits] })
-    );
-  }
+  const [hits, setHits] = useState([]);
+  const [newImput, setNewImput] = useState('cat');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [imageURL, setImageURL] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.newImput !== this.state.newImput ||
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ status: Status.PENDING });
-      getFetch(this.state.newImput, this.state.page)
-        .then(data => {
-          if (!data.hits.length) {
-            this.setState({ status: Status.REJECTED });
-            return;
-          }
-          this.setState(prev => ({
-            hits:
-              this.state.page > 1 ? [...prev.hits, ...data.hits] : data.hits,
-            status: Status.RESOLVED,
-          }));
-        })
+  // componentDidMount() {
+  //   getFetch(this.state.newImput, this.state.page).then(data =>
+  //     this.setState({ hits: [...data.hits] })
+  //   );
+  // };
 
-        .catch(error => this.setState({ error, status: Status.REJECTED }));
+  useEffect(() => {
+    if (!newImput) {
+      getFetch(newImput, page).then(data => setHits([...data.hits]));
+      setStatus(Status.PENDING);
+      return;
     }
-  }
 
-  onClickButton = () => {
-    // getFetch(this.state.newImput, this.state.page).then(data =>
-    this.setState(prev => ({
-      page: prev.page + 1,
-      status: Status.RESOLVED,
-    }));
+    getFetch(newImput, page)
+      .then(data => {
+        if (!data.hits.length) {
+          setStatus(Status.REJECTED);
+          return;
+        }
+
+        setHits(prev => (page > 1 ? [...prev, ...data.hits] : data.hits));
+        setStatus(Status.RESOLVED);
+      })
+      .catch(error => {
+        setError(error);
+        setStatus(Status.REJECTED);
+      });
+  }, [newImput, page]);
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (
+  //     prevState.newImput !== this.state.newImput ||
+  //     prevState.page !== this.state.page
+  //   ) {
+  //     this.setState({ status: Status.PENDING });
+  //     getFetch(this.state.newImput, this.state.page)
+  //       .then(data => {
+  //         if (!data.hits.length) {
+  //           this.setState({ status: Status.REJECTED });
+  //           return;
+  //         }
+  //         this.setState(prev => ({
+  //           hits:
+  //             this.state.page > 1 ? [...prev.hits, ...data.hits] : data.hits,
+  //           status: Status.RESOLVED,
+  //         }));
+  //       })
+
+  //       .catch(error => this.setState({ error, status: Status.REJECTED }));
+  //   }
+  // }
+
+  const onClickButton = () => {
+    setPage(prev => prev + 1);
+    setStatus(Status.RESOLVED);
   };
 
-  changeSearch = newImput => {
-    this.setState({ newImput, hits: [], page: 1 });
+  const changeSearch = newImput => {
+    setNewImput(newImput);
+    setHits([]);
+    setPage(1);
+
+    // this.setState({ newImput, hits: [], page: 1 });
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const toggleModal = () => {
+    setShowModal(showModal => !showModal);
   };
 
-  onModalOpen = largeImageURL => {
-    this.setState({ imageURL: largeImageURL });
-    this.toggleModal();
+  const onModalOpen = largeImageURL => {
+    setImageURL(largeImageURL);
+    toggleModal();
   };
 
-  render() {
-    const { hits, status, showModal, imageURL } = this.state;
+  // const { hits, status, showModal, imageURL } = this.state;
 
-    return (
+  return (
+    <>
+      <Searchbar changeSearch={changeSearch} />
+      {status === 'idle' ? (
+        <div className={s['title']}> Введите запрос ... </div>
+      ) : null}
+      {status === 'rejected' ? <div> Нет ответа по запросу</div> : null}
+
+      {status === 'pending' && <Loader />}
+
       <>
-        <Searchbar changeSearch={this.changeSearch} />
-        {status === 'idle' ? (
-          <div className={s['title']}> Введите запрос ... </div>
-        ) : null}
-        {status === 'rejected' ? <div> Нет ответа по запросу</div> : null}
-
-        {status === 'pending' && <Loader />}
-
-        <>
-          <ImageGallery carts={hits} onModalOpen={this.onModalOpen} />
-          <Button onClickButton={this.onClickButton} />
-          {showModal && (
-            <Modal onClose={this.toggleModal}>
-              <img
-                onClick={this.toggleModal}
-                src={imageURL}
-                alt=""
-                style={{
-                  width: '800px',
-                  // height: '100p',
-                }}
-              />
-            </Modal>
-          )}
-        </>
+        <ImageGallery carts={hits} onModalOpen={onModalOpen} />
+        <Button onClickButton={onClickButton} />
+        {showModal && (
+          <Modal onClose={toggleModal}>
+            <img
+              onClick={toggleModal}
+              src={imageURL}
+              alt=""
+              style={{
+                width: '800px',
+                // height: '100p',
+              }}
+            />
+          </Modal>
+        )}
       </>
-    );
-  }
+    </>
+  );
 }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,46 +1,46 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import s from './Searchbar.module.css';
-class Searchbar extends Component {
-  state = {
-    tags: '',
-  };
+function Searchbar({ changeSearch }) {
+  // state = {
+  //   tags: '',
+  // };
 
-  handleNameChange = event => {
+  const [tags, setTags] = useState('');
+
+  const handleNameChange = event => {
     console.log(event.currentTarget.value);
     const inputValue = event.currentTarget.value.trim();
 
-    this.setState({ tags: inputValue.toLowerCase() });
+    setTags(inputValue.toLowerCase());
   };
 
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
 
-    if (!this.state.tags) return;
-    this.props.changeSearch(this.state.tags);
+    if (!tags) return;
+    changeSearch(tags);
   };
 
-  render() {
-    return (
-      <>
-        <header className={s['searchbar']}>
-          <form className={s['form']} onSubmit={this.onSubmit}>
-            <button type="submit" className={s['button']}>
-              <span className={s['button-label']}>Search</span>
-            </button>
+  return (
+    <>
+      <header className={s['searchbar']}>
+        <form className={s['form']} onSubmit={onSubmit}>
+          <button type="submit" className={s['button']}>
+            <span className={s['button-label']}>Search</span>
+          </button>
 
-            <input
-              className={s['input']}
-              type="text"
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images and photos"
-              onChange={this.handleNameChange}
-            />
-          </form>
-        </header>
-      </>
-    );
-  }
+          <input
+            className={s['input']}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={handleNameChange}
+          />
+        </form>
+      </header>
+    </>
+  );
 }
 
 export default Searchbar;
